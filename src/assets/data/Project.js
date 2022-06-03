a3ced874e506ac688d8c21b39d9a12c75df394f5
Hunga9k50doker <19520101@gm.uit.edu.vn>
2022-06-03 23:58:55 +0700

    fix ui tablet

--- src/assets/data/Project.js
+++ src/assets/data/Project.js
@@ -7,18 +7,6 @@ import {
   project_2_img_01,
   project_2_img_02,
   project_2_img_03,
-  project_2_img_04,
-  project_2_img_05,
-  // project_3_img_01,
-  // project_3_img_02,
-  // project_3_img_03,
-  // project_3_img_04,
-  // project_4_img_01,
-  // project_4_img_02,
-  // project_4_img_03,
-  // project_5_img_01,
-  // project_5_img_02,
-  // project_5_img_03,
 } from "../img";
 
 const arrProj = [
@@ -40,30 +28,13 @@ const arrProj = [
   {
     title: "Diamond",
     img: project_2_img_01,
-    thumb: [
-      project_2_img_01,
-      project_2_img_02,
-      project_2_img_03,
-      project_2_img_04,
-      project_2_img_05,
-    ],
+    thumb: [project_2_img_01, project_2_img_02, project_2_img_03],
     path: "https://delicate-llama-f9bfad.netlify.app/",
     desc: "This is a website that sells jewelry. Features like login, register, adding, editing, deleting, searching, add review, filter, order management, invoice details ...",
     frontend: "HTML , JSX , JS, CSS(SCSS), ReactJs ",
     backend: "Nodejs-ExpressJs, MongoDB",
     otherDesc: "Nothing!!!",
   },
-
-  // {
-  //   title: "Diamond",
-  //   img: project_4_img_01,
-  //   thumb: [project_4_img_01, project_4_img_02, project_4_img_03],
-  //   path: "/",
-  //   desc: "This project is under development.",
-  //   frontend: "Update soon.",
-  //   backend: "Update soon..",
-  //   otherDesc: "Nothing!.!!",
-  // },
 ];
 
 export default arrProj;
